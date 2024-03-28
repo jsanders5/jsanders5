e4a108897a6126d4505f72dae61eea2599df8d67
Jake <jtsanders87@gmail.com>
2024-03-28 11:18:51 -0600

    fix issue in zsh install script

--- mac/scripts/omz/install-zsh.sh
+++ mac/scripts/omz/install-zsh.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
+#!/bin/zsh
 
 [[ -x $HOME/.oh-my-zsh ]] || sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 source ~/.zshrc
 
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+source ~/.zshrc
